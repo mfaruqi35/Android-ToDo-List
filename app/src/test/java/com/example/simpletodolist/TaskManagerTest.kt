93d22bca529b0c5106ee287b403b6f7af66d8166
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-04-09 16:49:24 +0700

    add comment

--- app/src/test/java/com/example/simpletodolist/TaskManagerTest.kt
+++ app/src/test/java/com/example/simpletodolist/TaskManagerTest.kt
@@ -9,29 +9,34 @@ class TaskManagerTest {
 
     private lateinit var manager: TaskManager
 
+    /* Inisialisasi TaskManager baru sebelum mulai setiap test */
     @Before
     fun setup(){
         manager = TaskManager()
     }
 
+    /* Unit test untuk memastikan list bertambah 1 setelah task valid ditambahkan*/
     @Test
     fun `tambah task valid menambah ukuran list`(){
         manager.addTask("Belajar KPL")
         assertEquals(1, manager.getTasks().size)
     }
 
+    /* Unit test untuk memastikan task dengan judul spasi/kosong tidak masuk ke list*/
     @Test
     fun `tambah task kosong tidak menambah ukuran list`(){
         manager.addTask(" ")
         assertEquals(0, manager.getTasks().size)
     }
 
+    /* Unit test untuk memastikan addTask() mengembalikan false jika judul kosong */
     @Test
     fun `tambah task kosong kembalikan false`(){
         val result = manager.addTask(" ")
         assertFalse(result)
     }
 
+    /* Unit test untuk memastikan task benar-beanr hilang dari list setelah dihapus*/
     @Test
     fun `menghapus task menghilangkannya dari list`(){
         manager.addTask("Belajar KPL")
@@ -40,6 +45,7 @@ class TaskManagerTest {
         assertTrue(manager.getTasks().isEmpty())
     }
 
+    /* Unit test untuk memastikan isDone berubah menjadi true setelah completeTask dipanggil*/
     @Test
     fun `ceklis task membuat isDone task menjadi true`(){
         manager.addTask("Belajar KPL")
@@ -48,6 +54,7 @@ class TaskManagerTest {
         assertTrue(manager.getTasks().first().isDone)
     }
 
+    /* Unit test untuk memastikan isDone kembali false jika completeTask dipanggil dua kali (toggle)*/
     @Test
     fun `unceklis task membuat isDone task menjadi false`(){
         manager.addTask("Belajar KPL")
@@ -58,6 +65,7 @@ class TaskManagerTest {
 
     }
 
+    /* Unit test untuk memastikan semua task tersimpan dengan benar saat ditambahkan sekaligus*/
     @Test
     fun `menambah banyak task`(){
         manager.addTask("Task 1")
