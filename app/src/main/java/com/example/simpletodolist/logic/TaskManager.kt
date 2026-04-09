935cfc48ad25e8d605de9e8a7c9974df36a2affc
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-04-09 12:53:03 +0700

    add TaskManager for logic

--- app/src/main/java/com/example/simpletodolist/logic/TaskManager.kt
+++ app/src/main/java/com/example/simpletodolist/logic/TaskManager.kt
@@ -1,4 +1,27 @@
 package com.example.simpletodolist.logic
 
+import com.example.simpletodolist.model.Task
+
 class TaskManager {
+    private val _tasks = mutableListOf<Task>()
+    private var nextId = 1
+
+    fun addTask(title: String): Boolean {
+        if (title.isBlank()) return false
+        _tasks.add(Task(nextId++, title))
+        return true
+    }
+
+    fun deleteTask(id: Int){
+        _tasks.removeIf { it.id == id }
+    }
+
+    fun completeTask(id: Int) {
+        val index = _tasks.indexOfFirst { it.id == id }
+        if (index != -1) {
+            _tasks[index] = _tasks[index].copy(isDone = !_tasks[index].isDone)
+        }
+    }
+
+    fun getTasks(): List<Task> = _tasks.toList()
 }
